3b23ab2149390e82d89daf6df933d7156b2b0b7a
Peter-korn <peterkorn2006@gmail.com>
2025-04-27 23:00:45 +0300

    input, output dir logic

--- collect_files.sh
+++ collect_files.sh
@@ -24,3 +24,16 @@ copyFile() {
 
     cp "$filePath" "$target"
 }
+
+if [ "$#" -ne 2 ]; then
+    exit 1
+fi
+
+input_dir="$1"
+output_dir="$2"
+
+if [ ! -d "$input_dir" ]; then
+    exit 1
+fi
+
+mkdir -p "$output_dir"
